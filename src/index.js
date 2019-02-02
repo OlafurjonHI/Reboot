b851c720d51d562ca3bc5b9e86e9bc84c0ba312b
OlafurjonHI <ojv1@hi.is>
2019-02-02 15:36:38 +0000

    Pushing

--- src/index.js
+++ src/index.js
@@ -3,13 +3,5 @@ import Lecture from './lib/lecture';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
-  const isLecturePage = page.classList.contains('lecture-page');
 
-  if (isLecturePage) {
-    const lecture = new Lecture();
-    lecture.load();
-  } else {
-    const list = new List();
-    list.load();
-  }
 });
